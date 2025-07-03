6e3298bda89631e2dc18b69f98bd79c859c28159
unknown <esperanzatm95@mail.com>
2025-07-03 20:52:24 +0300

    authentification feature

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,13 @@ use App\Http\Controllers\EmployeeController;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::middleware('auth')->group(function () {
 Route::get('/employees', [EmployeeController::class, 'index']);
 Route::get('/employees/create', [EmployeeController::class, 'create']);
 Route::post('/employees', [EmployeeController::class, 'store']);
 Route::get('/employees/{employee}/edit', [EmployeeController::class, 'edit']);
 Route::put('/employees/{employee}', [EmployeeController::class, 'update']);
 Route::delete('/employees/{employee}', [EmployeeController::class, 'destroy']);
+});
+
+require __DIR__ . '/auth.php';
